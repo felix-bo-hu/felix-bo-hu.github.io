9072a4bde7d9cd784c1c20cc427606c875414012
felix-bo-hu <35824496+felix-bo-hu@users.noreply.github.com>
2018-01-26 10:15:48 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/robot-312566_960_720.png') {
+      myImage.setAttribute ('src','images/robot-161368_960_720.png');
+    } else {
+      myImage.setAttribute ('src','images/robot-312566_960_720.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
